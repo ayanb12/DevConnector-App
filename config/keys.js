3db2123c14bc9f3d15a1d6e288caa70d2b296386
Ayan Banerjee <ayanbbb354@gmail.com>
2019-03-27 22:46:41 +0530

    Done basic authentication with passport and JWT, and added validations form login and register

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb+srv://ayan12:ayan12@devconnector-xvuit.mongodb.net/test?retryWrites=true'
+	mongoURI: 'mongodb+srv://ayan12:ayan12@devconnector-xvuit.mongodb.net/test?retryWrites=true',
+	secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport')
+
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
 
 const app = express()
+
+//Body parser
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use(bodyParser.json())
 
 //DB config
 const db = require('./config/keys').mongoURI;
@@ -19,7 +28,12 @@ mongoose.connect(db, {
 	})
 	.catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello world!'));
+
+//passport middleware
+app.use(passport.initialize());
+
+//passport config
+require('./config/passport')(passport)
 
 //use Routes
 app.use('/api/users', users);
