9bc225a6422af213059a3144eba3f243f327d1bd
Ayan Banerjee <ayanbbb354@gmail.com>
2019-04-13 18:28:35 +0530

    Prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-	mongoURI: 'mongodb+srv://ayan12:ayan12@devconnector-xvuit.mongodb.net/test?retryWrites=true',
-	secretOrKey: 'secret'
+if(process.env.NODE_ENV === 'production') {
+	module.exports = require('./keys_prod')
+} else {
+	module.exports = require('./keys_dev');
 }
